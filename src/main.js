09ff73403e31393af96ce55861edda2654a4452d
hiroya232 <mowoarm232@gmail.com>
2020-12-31 11:08:34 +0900

    [fix]:main.js復元
    main.jsの中身が消えていたため復元

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World from your main file!');
+import Vue from "vue";
+import App from "./App.vue";
+import VueClipboard from "vue-clipboard2";
+
+Vue.config.productionTip = false;
+
+Vue.use(VueClipboard);
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
