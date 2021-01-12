84d099867589273aa034a5623090a4cb771a138e
hiroya232 <mowoarm232@gmail.com>
2021-01-12 21:22:00 +0900

    [clean]:MultiList.vueとCoopMultiList.vueのコード分割　途中
    共通部分をGetCoopTweet.vueに切り出し

--- src/components/CoopMultiList.vue
+++ src/components/CoopMultiList.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
-    <button @click="getTweets">共闘マルチ検索</button>
-    <div
-      class="coop-multi-list"
-      v-for="coopMulti in coopMultiList"
-      :key="coopMulti.id"
-      v-clipboard:copy="coopMulti.id"
-      v-clipboard:success="onCopy"
-      v-clipboard:error="onError"
-    >
-      <div>{{ coopMulti.info }}</div>
-      <div>{{ coopMulti.id }}</div>
-    </div>
+    <GetMultiTweet
+      multiType="共闘マルチ"
+      search-keyword="［グラブル］マルチバトル参加者募集！"
+    ></GetMultiTweet>
   </div>
 </template>
 
 <script>
-import Twitter from "twitter";
+import GetMultiTweet from "./GetMultiTweet.vue";
 
 export default {
   data: function () {
@@ -24,55 +16,8 @@ export default {
       coopMultiList: [],
     };
   },
-  methods: {
-    getTweets() {
-      let client = new Twitter({
-        consumer_key: process.env.VUE_APP_CONSUMER_KEY,
-        consumer_secret: process.env.VUE_APP_CONSUMER_SECRET,
-        access_token_key: process.env.VUE_APP_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.VUE_APP_ACCESS_TOKEN_SECRET,
-      });
-
-      //ツイート取得
-      let self = this;
-      client.stream(
-        "statuses/filter",
-        { track: "［グラブル］マルチバトル参加者募集！" },
-        function (stream) {
-          stream.on("data", function (tweet) {
-            let splittedTweet = tweet.text.split(/[ \n]/);
-            let coopInfo = splittedTweet.slice(-1)[0];
-            let coopId = splittedTweet.slice(0, 1)[0].split("：")[0];
-            // console.log(tweet);
-            console.log(splittedTweet);
-            console.log(coopInfo);
-            console.log(coopId);
-
-            //データ加工
-            let coopMultiInfos = {
-              id: coopId,
-              info: coopInfo,
-            };
-
-            self.coopMultiList.splice(0, 0, coopMultiInfos);
-
-            if (self.coopMultiList.length > 10) {
-              self.coopMultiList.splice(10, 1);
-            }
-          });
-
-          stream.on("error", function (error) {
-            console.log(error);
-          });
-        }
-      );
-    },
-    onCopy: function (id) {
-      alert("You just copied: " + id.text);
-    },
-    onError: function () {
-      alert("Failed to copy texts");
-    },
+  components: {
+    GetMultiTweet,
   },
 };
 </script>
